750a0a2b25700d426c5dc3d46ce27710d9574a33
bearmug <pavel.fadeev@gmail.com>
2016-06-12 23:17:25 +0300

    pick a joke from library module

--- FinalProject/joke-lib/src/main/java/com/udacity/gradle/builditbigger/JokeLibrary.java
+++ FinalProject/joke-lib/src/main/java/com/udacity/gradle/builditbigger/JokeLibrary.java
@@ -1,4 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
 public class JokeLibrary {
+
+    private static final String SINGLE_JOKE = "Long hours - green mouse";
+
+    public String pickJoke() {
+        return SINGLE_JOKE;
+    }
 }
