ee310b133d06cd9e8186295b88257fc22b55e238
bearmug <pavel.fadeev@gmail.com>
2016-06-17 01:11:04 +0300

    split free/paid activities

---
+++ FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragmentPaid.java
@@ -0,0 +1,13 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+}
